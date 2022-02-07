89adbb3c700f7a845b7c70dc837dbc5d7c836aac
makayasa <makayasa@ozip.co.id>
2022-02-07 11:20:18 +0700

    bagian 5 end

--- src/messages/dtos/create-message.dto.ts
+++ src/messages/dtos/create-message.dto.ts
@@ -3,9 +3,9 @@ import { IsString, IsNumber, IsOptional } from 'class-validator';
 export class CreateMessageDto {
   @IsString()
   @IsOptional()
-  content: String;
-  @IsString()
-  nama: String;
-  @IsNumber()
-  age: number;
+  content: string;
+  // @IsString()
+  // nama: string;
+  // @IsNumber()
+  // age: number;
 }
